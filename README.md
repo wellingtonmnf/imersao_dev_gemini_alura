7a143663a17d3a4d6a1a6f4bf216e9d292b62eaf
wellingtonmnf <wellingtonmnf@outlook.com>
2024-09-04 17:46:00 -0300

    Aula 03 finalziada

--- README.md
+++ README.md
@@ -2,3 +2,11 @@
 
 Este projeto foi criado durante a Imersão Dev_ com Gemini realizada pela Alura,
 em setembro de 2024
+
+* Aulas (02 a 06/09/2024): 
+
+[x] Aula 01: Iniciando o Projeto com HTML e CSS
+[x] Aula 02: Estilizando o projeto com CSS
+[x] Aula 03: Criando a base de dados com JavaScript
+[ ] Aula 04: Implementando a funcionalidade de busca
+[ ] Aula 05:Finalizando o projeto com GitHub e Deploy
---
+++ dados.js
@@ -0,0 +1,17 @@
+let dados = [
+    {
+        titulo: "Rayssa Leal",
+        descricao: "Rayssa Leal, a 'Fadinha', é uma skatista brasileira que conquistou o mundo com seu talento e carisma. Aos poucos anos, ela se tornou um dos maiores nomes do skate mundial, inspirando jovens e quebrando barreiras no esporte.",
+        link: "https://pt.wikipedia.org/wiki/Rayssa_Leal"
+    },
+    {
+        titulo: "Beatriz Souza",
+        descricao: "Beatriz Souza é uma judoca brasileira que se destacou nas competições nacionais e internacionais. Sua dedicação e técnica fizeram dela uma referência no judô feminino brasileiro.",
+        link: "https://pt.wikipedia.org/wiki/Beatriz_Souza" 
+    },
+    {
+        titulo: "Rebeca Andrade",
+        descricao: "Rebeca Andrade é uma ginasta brasileira que conquistou medalhas olímpicas e se tornou um dos maiores nomes da ginástica artística mundial. Sua força e determinação inspiraram milhões de pessoas.",
+        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade" 
+    }
+];
